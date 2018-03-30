3663623b0cddba15e043ba91986c9dd5bb7a10d9
zhaoming <zhaoming@mallcoo.cn>
2018-03-30 14:28:41 +0800

    查看作品列表

--- src/client/web/routes/index.js
+++ src/client/web/routes/index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var request = require('request');
+var util = require('../util/utils');
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
@@ -14,6 +15,12 @@ router.get('/gifmaker', function (req, res, next) {
   request("https://gifmaker.develophelper.com/gif/category", function (error, response, body) {
     res.render('gifmaker', { title: '字幕制作', data: JSON.parse(body).d[index] });
   });
+});
+
+router.get('/opus', function (req, res, next) {
+  var fileList = util.getFileList('../../server/public/cache/');
+  console.log(fileList);
+  res.render('opus', { title: '字幕制作', data: fileList });
 });
 
 module.exports = router;
---
+++ src/client/web/util/utils.js
@@ -0,0 +1,27 @@
+var fs = require('fs');
+
+exports.getFileList = function (path) {
+    var filesList = [];
+    readFileList(path, filesList);
+    return filesList;
+}
+
+function readFileList(path, filesList) {
+    var files = fs.readdirSync(path);
+    files.forEach(function (itm, index) {
+        var stat = fs.statSync(path + itm);
+        console.log(stat);
+        if (stat.isDirectory()) {
+            //递归读取文件
+            readFileList(path + itm + "/", filesList)
+        } else {
+
+            var obj = {};//定义一个对象存放文件的路径和名字
+            obj.path = path;//路径
+            obj.filename = itm;//名字
+            obj.ctime = stat.ctime
+            filesList.push(obj);
+        }
+    })
+}
+
